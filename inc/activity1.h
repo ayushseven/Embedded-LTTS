02b03ecd4d4996cb9c739e5a15831ca84686db8f
ayushseven <86160630+ayushseven@users.noreply.github.com>
2021-07-24 21:44:49 +0530

    Create activity1.h

---
+++ inc/activity1.h
@@ -0,0 +1,9 @@
+#ifndef __ACTIVITY1_H__
+#define __ACTIVITY1_H__
+
+#include <avr/io.h>     // HEADER FILE FOR ATMEGA328
+
+void act1(void);   // IMPLEMENTATION OF ACTIVITY 1
+void initialise_gpiopins(void);   // INITIALIZATION OF GPIO PINS
+
+#endif
